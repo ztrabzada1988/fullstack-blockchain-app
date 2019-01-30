454e0a193b45374ab0616708376d164be6f32931
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-01-29 22:12:46 -0800

    update block

--- block.js
+++ block.js
@@ -1,31 +1,31 @@
-const { GENESIS_DATA } = require('./config');
-const cryptoHash = require('./crypto-hash');
+const hexToBinary = require('hex-to-binary');
+const { GENESIS_DATA, MINE_RATE } = require('../config');
+const { cryptoHash } = require('../util');
 
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) { // these 4 is what a block contains. When you wrap in {} the order wont matter
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) { // these 4 is what a block contains. When you wrap in {} the order wont matter
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
+  }
 
-    // genesis is the first block of the blockchain
-    static genesis() { // static methods aren't called on instances of a class. They are utility functions within a class
-        return new this(GENESIS_DATA); // this is same as Block but in static func.
-    }
+  // genesis is the first block of the blockchain
+  static genesis() { // static methods aren't called on instances of a class. They are utility functions within a class
+    return new this(GENESIS_DATA); // this is same as Block but in static func.
+  }
 
-    static mineBlock({ lastBlock, data }) {
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
+  static mineBlock({ lastBlock, data }) {
+    const lastHash = lastBlock.hash;
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
 
-        return new this({
-            timestamp,
-            lastHash,
-            data,
-            hash: cryptoHash(timestamp, lastHash, data)
-        });
-    }
+
 }
 
 module.exports = Block;
+
 
