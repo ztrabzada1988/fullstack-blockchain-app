8b8889637c45f519333cc2ceb3cb0d7e30e43ed7
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-01-30 23:50:41 -0800

    update blockchain test file

--- blockchain.test.js
+++ blockchain.test.js
@@ -33,5 +33,51 @@ describe('Block', () => {
     });
   });
 
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it('sets a `timestamp`', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it('creates a SHA-256 `hash` based on the proper inputs', () => {
+      expect(minedBlock.hash)
+        .toEqual(
+          cryptoHash(
+            minedBlock.timestamp,
+            minedBlock.nonce,
+            minedBlock.difficulty,
+            lastBlock.hash,
+            data
+          )
+        );
+    });
+
+    it('sets a `hash` that matches the difficulty criteria', () => {
+      expect(hexToBinary(minedBlock.hash).substring(0, minedBlock.difficulty))
+        .toEqual('0'.repeat(minedBlock.difficulty));
+    });
+
+    it('adjusts the difficulty', () => {
+      const possibleResults = [lastBlock.difficulty+1, lastBlock.difficulty-1];
+
+      expect(possibleResults.includes(minedBlock.difficulty)).toBe(true);
+    });
+  });
+
   
 });
