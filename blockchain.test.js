dd1ed4e74b397707eb0304ca1ec11d4e8c942844
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-01-30 23:50:14 -0800

    update blockchain test file

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,21 +1,37 @@
-const Blockchain = require('./blockchain');
+const hexToBinary = require('hex-to-binary');
 const Block = require('./block');
+const { GENESIS_DATA, MINE_RATE } = require('../config');
+const { cryptoHash } = require('../util');
 
-describe('Blockchain', () => {
-    const blockchain = new Blockchain();
+describe('Block', () => {
+  const timestamp = 2000;
+  const lastHash = 'foo-hash';
+  const hash = 'bar-hash';
+  const data = ['blockchain', 'data'];
+  const nonce = 1;
+  const difficulty = 1;
+  const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty });
 
-    it('contains a `chain` Array instance', () => {
-        expect(blockchain.chain instanceof Array).toBe(true);
+  it('has a timestamp, lastHash, hash, and data property', () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+    expect(block.data).toEqual(data);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
+  });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
     });
 
-    it('starts with the genesis block', () => {
-        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
     });
+  });
 
-    it('adds a new block to the chain', () => {
-        const newData = 'foo bar';
-        blockchain.addBlock({ data: newData });
-
-        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
-    });
+  
 });
