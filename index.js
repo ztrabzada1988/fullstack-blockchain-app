b4b19b054fe1509b1f8ab83a532e5b419d56bf46
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-02-03 13:54:40 -0800

    post request complete. API complete

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // middleware for express
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, res) => { // read data from the backend .get is from express
     res.json(blockchain.chain); // this will send the blockchain.chain to whoever makes the get request with /api/blocks endpoint
 }); 
 
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body; // destructuring the data field from body request that will be coming in 
+
+    blockchain.addBlock({ data }); // this will add a new block with the data from the requester
+
+    res.redirect('/api/blocks'); // this will send you back to the /api/blocks endpoint to see if your block was added 
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening at localhost: ${PORT}`));
